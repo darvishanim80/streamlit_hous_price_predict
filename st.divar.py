239a9cb5df251e171ea5c21dbffa4fc71da2716f
Mohammad <darvishanim80@gamil.com>
2026-04-26 12:38:48 -0700

    delet the line 4

--- st.divar.py
+++ st.divar.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd 
 import numpy as np
-import folium
 from streamlit_folium import st_folium
 from sklearn.experimental import enable_iterative_imputer 
 from sklearn.ensemble import RandomForestRegressor
